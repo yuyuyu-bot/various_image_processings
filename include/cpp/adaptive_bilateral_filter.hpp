b7d6be1de83a918a96709b2100076836ee5d2dbc
yuki.tanaka <yuyuyu.nttmmoo@gmail.com>
2023-01-21 17:57:27 +0900

    y -> r

--- include/cpp/adaptive_bilateral_filter.hpp
+++ include/cpp/adaptive_bilateral_filter.hpp
@@ -9,6 +9,8 @@
 
 namespace {
 
+namespace internal {
+
 class IntegralImage {
 public:
     IntegralImage(
@@ -16,7 +18,6 @@ public:
         const int radius)
     : width_(src.cols + radius * 2 + 1),
       height_(src.rows + radius * 2 + 1),
-      channels_(src.channels()),
       radius_(radius) {
         const int dim[3] = { height_, width_, channels_};
         buffer_.create(src.dims, dim);
@@ -58,12 +59,14 @@ public:
     }
 
 private:
+    static constexpr int channels_ = 3;
     const int width_;
     const int height_;
-    const int channels_;
     const int radius_;
     cv::Mat3i buffer_;
 };
+
+} // namespace internal
 
 inline void adaptive_bilateral_filter(
     const cv::Mat3b& src,
@@ -72,27 +75,6 @@ inline void adaptive_bilateral_filter(
     const float sigma_space = 10.f,
     const float sigma_color = 30.f
 ) {
-    dst.create(src.size());
-    const auto width = src.cols;
-    const auto height = src.rows;
-    const auto radius = ksize / 2;
-
-    const auto [kernel_space, kernel_color_table] = pre_compute_kernels<512 * 3>(ksize, sigma_space, sigma_color);
-
-    const auto get_kernel_space = [ksize, radius, kernel_space](const int kx, const int ky) {
-        return kernel_space[(ky + radius) * ksize + (kx + radius)];
-    };
-
-    const auto get_kernel_color = [kernel_color_table](const cv::Vec3b& a, const cv::Vec3b& b, cv::Vec3i& offset) {
-        const auto diff0 = static_cast<int>(a[0]) - static_cast<int>(b[0]) - offset[0];
-        const auto diff1 = static_cast<int>(a[1]) - static_cast<int>(b[1]) - offset[1];
-        const auto diff2 = static_cast<int>(a[2]) - static_cast<int>(b[2]) - offset[2];
-        const auto color_distance = std::abs(diff0) + std::abs(diff1) + std::abs(diff2);
-        return kernel_color_table[color_distance];
-    };
-
-    // IntegralImage src_integral(src, radius);
-
     struct AdaptiveBilateralFilterCore : cv::ParallelLoopBody {
         AdaptiveBilateralFilterCore(
             const cv::Mat3b& src,
@@ -125,14 +107,12 @@ inline void adaptive_bilateral_filter(
         void operator()(const cv::Range& range) const CV_OVERRIDE {
             for (int r = range.start; r < range.end; r++) {
                 for (int x = 0; x < width_; x++) {
-                    const auto y = r;
-
-                    const auto src_center_pix = src_.at<cv::Vec3b>(y, x);
-                    const auto src_sum = src_integral_.get(x - radius_, y - radius_, x + radius_, y + radius_);
+                    const auto src_center_pix = src_.at<cv::Vec3b>(r, x);
+                    const auto src_sum = src_integral_.get(x - radius_, r - radius_, x + radius_, r + radius_);
                     const auto del0 = src_center_pix[0] - src_sum[0] / (ksize_ * ksize_);
                     const auto del1 = src_center_pix[1] - src_sum[1] / (ksize_ * ksize_);
                     const auto del2 = src_center_pix[2] - src_sum[2] / (ksize_ * ksize_);
-                    cv::Vec3i offset(-del0, -del1, -del2);
+                    cv::Vec3i offset(del0, del1, del2);
 
                     auto sum0 = 0.f;
                     auto sum1 = 0.f;
@@ -141,9 +121,9 @@ inline void adaptive_bilateral_filter(
                     for (int ky = -radius_; ky <= radius_; ky++) {
                         for (int kx = -radius_; kx <= radius_; kx++) {
                             const auto x_clamped = std::clamp(x + kx, 0, width_ - 1);
-                            const auto y_clamped = std::clamp(y + ky, 0, height_ - 1);
+                            const auto y_clamped = std::clamp(r + ky, 0, height_ - 1);
                             const auto src_pix   = src_.at<cv::Vec3b>(y_clamped, x_clamped);
-                            const auto kernel    = get_kernel_space_(kx, ky) * get_kernel_color_(src_center_pix, src_pix, offset);
+                            const auto kernel    = get_kernel_space_(kx, ky) * get_kernel_color_(src_pix, src_center_pix, offset);
 
                             sum0 += src_pix[0] * kernel;
                             sum1 += src_pix[1] * kernel;
@@ -152,9 +132,9 @@ inline void adaptive_bilateral_filter(
                         }
                     }
 
-                    dst_.at<cv::Vec3b>(y, x)[0] = static_cast<std::uint8_t>(sum0 / sumk + 0.5f);
-                    dst_.at<cv::Vec3b>(y, x)[1] = static_cast<std::uint8_t>(sum1 / sumk + 0.5f);
-                    dst_.at<cv::Vec3b>(y, x)[2] = static_cast<std::uint8_t>(sum2 / sumk + 0.5f);
+                    dst_.at<cv::Vec3b>(r, x)[0] = static_cast<std::uint8_t>(sum0 / sumk + 0.5f);
+                    dst_.at<cv::Vec3b>(r, x)[1] = static_cast<std::uint8_t>(sum1 / sumk + 0.5f);
+                    dst_.at<cv::Vec3b>(r, x)[2] = static_cast<std::uint8_t>(sum2 / sumk + 0.5f);
                 }
             }
         }
@@ -166,7 +146,7 @@ inline void adaptive_bilateral_filter(
         const int ksize_;
         const int radius_;
 
-        const IntegralImage src_integral_;
+        const internal::IntegralImage src_integral_;
         std::vector<float> kernel_space_;
         std::array<float, 512 * 3> kernel_color_table_;
         std::function<float(int, int)> get_kernel_space_;
